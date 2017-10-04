80eb55a4380a0d8eff4b44230f57dd1f56367ed1
jatinpruthi21 <jatinpruthi21@gmail.com>
2017-10-05 03:09:42 +0530

    initial commit

---
+++ app/src/main/java/projects/jatin/quizme/Model/Splash.java
@@ -0,0 +1,15 @@
+package projects.jatin.quizme.Model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import projects.jatin.quizme.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}

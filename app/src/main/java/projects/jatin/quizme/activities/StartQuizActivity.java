6d2088407ec6224c2fddfe225954eb42fba24b19
jatinpruthi21 <jatinpruthi21@gmail.com>
2017-11-13 05:08:37 +0530

    added questions in firebase database

---
+++ app/src/main/java/projects/jatin/quizme/activities/StartQuizActivity.java
@@ -0,0 +1,15 @@
+package projects.jatin.quizme.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import projects.jatin.quizme.R;
+
+public class StartQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_quiz);
+    }
+}
---
+++ app/src/main/java/projects/jatin/quizme/common/Common.java
@@ -0,0 +1,13 @@
+package projects.jatin.quizme.common;
+
+import projects.jatin.quizme.model.User;
+
+/**
+ * Created by Jateen on 12-11-2017.
+ */
+
+public class Common {
+
+    public static String categoryId;
+    public static User currentUser;
+}
